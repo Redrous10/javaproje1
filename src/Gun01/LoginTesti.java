03fba567c0df2b30597ce9b8dc11ddc9ec89d3ac
hp <semihugras@hotmail.com>
2025-03-02 05:42:07 +0300

    Yeni login testi yapıldı

--- src/Gun01/LoginTesti.java
+++ src/Gun01/LoginTesti.java
@@ -3,10 +3,9 @@ package Gun01;
 public class LoginTesti {
     public static void main(String[] args) {
         // login testi için yeni şifre modeline göre yazıldı
-        System.out.println("Yeni Login Testi");
-        // çok satır yazdınm bitirdim
-        System.out.println("Login Testi 1");
-        System.out.println("sepet testi yazıldı bitirildi 3 günlük kodyazıldı");
+        System.out.println("Yeni Login Testi  1");
 
+        System.out.println("sepet testi yazıldı bitirildi 3 günlük kodyazıldı");
+        System.out.println("Yeni login testi yapıldı  7 gün uğraştım ");
     }
 }
